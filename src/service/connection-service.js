01ec8fa3a0b814489e3c7d3e273864a5305ab22c
test <test@qq.com>
2024-04-08 21:13:15 +0800

    指挥matemask钱包去添加当前项目所支持的网络

--- src/service/connection-service.js
+++ src/service/connection-service.js
@@ -2,8 +2,12 @@ import { ethers } from "ethers";
 import { messageBox } from "../service/message-service"
 import { configuration } from '../config'
 
+// 我们的应用其实可以配置很多他支持的网络
+// 可以强制指挥matemask钱包去添加这个应用支持的网络
+
+// 链接一次
 export const connectOnce = async () => {
-    debugger;
+    // debugger;
     let provider = new ethers.providers.Web3Provider(window.ethereum)
     await provider.send("eth_requestAccounts", []);
     let signer = provider.getSigner();
@@ -11,11 +15,13 @@ export const connectOnce = async () => {
     let address = await signer.getAddress();
     return { chainId: network.chainId, address: address, provider, signer };
 }
+// 尝试链接
 export const trying = async () => {
     const { chainId, address, provider, signer } = await connectOnce();
+    // 是不是我所支持的配置的网络id
     const supported = configuration().chainId.toString();
     if (chainId == supported) {
-        messageBox("success", "", 'chainId: ' + chainId + "      account: " + address.substring(0, 5) + "..")
+        messageBox("success", "", 'chainId: ' + chainId + "      account: " + address + "..")
 
         return { success: true, provider, signer };
     }
@@ -23,10 +29,13 @@ export const trying = async () => {
 
     return { success: false };
 }
+// 如果没有配置 会请求配置对应的网络链条
 export const connect = async () => {
-    let { success } = await trying();
+    let { success } = await trying();//成功的意思是 matemask钱包 发送了一个链接请求 并且根据返回的信息 应用确认
+    // 链接的网络是否是我们应该当中支持的网络
     if (success)
         return;
+    // 如果失败 请求钱包 配置当前应用所需要的网络配置
     const conf = configuration()
     await window.ethereum.request({
         method: "wallet_addEthereumChain",
