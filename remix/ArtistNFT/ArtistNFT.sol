c7109f2badbae21226ed97a0c06b083bef3097f1
test <test@qq.com>
2024-04-09 13:40:23 +0800

    调整

--- remix/ArtistNFT/ArtistNFT.sol
+++ remix/ArtistNFT/ArtistNFT.sol
@@ -2,9 +2,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/v4.8.2/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/v4.8.2/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.2/contracts/utils/Counters.sol";
+
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.8.2/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.8.2/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.8.2/contracts/utils/Counters.sol";
+
+
 
 contract ArtistNFT is ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
@@ -14,14 +22,14 @@ contract ArtistNFT is ERC721URIStorage, ERC721Enumerable {
        
     }
 
-    function mint(address artist, string memory tokenURI)
+    function mint(address artist, string memory _tokenURI)
         public
         returns (uint256)
     {
   
         uint256 newItemId = _tokenIds.current();
         _mint(artist, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, _tokenURI);
 
         _tokenIds.increment();
         return newItemId;
