33445e853c0ff03c9a3588746be57799111e80bd
test <test@qq.com>
2024-05-02 15:20:40 +0800

    注释

--- src/service/arweave-service.js
+++ src/service/arweave-service.js
@@ -13,6 +13,7 @@ export const toArweave = async (entity, tags) => {
         tx.addTag(k, tags[k]);
     })
     tx.addTag("Content-Type", "text/json");
+    // 背后调用钱包 调用那个钱包 看背后初始化的是插件钱包还是web钱包
     await arweave.transactions.sign(tx);
     const response = await arweave.transactions.post(tx);
 
